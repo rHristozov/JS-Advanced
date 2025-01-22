26a49bcadfe84e3f11009aba19c89948f4e71013
Roman Hristozov <roman.hristozov@gmail.com>
2025-01-22 12:58:13 +0200

    3 exercises

--- 04.Dom Introduction - Exercises/04. Search in List/search.js
+++ 04.Dom Introduction - Exercises/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   const listLi = document.querySelectorAll('#towns li');
+   const input = document.getElementById('searchText').value;
+   const result = document.getElementById('result');
+
+   let count = 0;
+
+   for (const li of listLi) {
+      if(li.textContent.includes(input)) {
+         li.style.fontWeight = 'bold';
+         li.style.textDecoration = 'underline';
+         count++;
+      } else {
+         li.style.fontWeight = '';
+         li.style.textDecoration = '';
+      }
+   }
+   result.textContent = `${count} matches found`
 }
--- 04.Dom Introduction - Exercises/05. Table - Search Engine/solution.js
+++ 04.Dom Introduction - Exercises/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   const tableRows = document.querySelectorAll('.container tbody tr');
 
    function onClick() {
-      //   TODO:
+      const inputText = document.getElementById('searchField');
 
+      for (const row of tableRows) {
+         if(row.textContent.includes(inputText.value)) {
+            row.classList.add('select');
+         } else {
+            row.classList.remove('select');
+         }
+      }      
+      inputText.value = '';
    }
 }

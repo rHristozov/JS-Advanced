d965d75bc8266bd18e8eb2892386d75ed6e23885
Roman Hristozov <roman.hristozov@gmail.com>
2025-01-29 22:28:36 +0200

    Pushing some exercises and new folders

---
+++ 06.Advanced Functions - Lab/01.areaAndVolumeCalculator.js
@@ -0,0 +1,34 @@
+function solve(area, vol, input) {
+  let objects = JSON.parse(input);
+  console.log(objects);
+
+  function calc(obj) {
+    let areaObj = Math.abs(area.call(obj));
+    let volumeObj = Math.abs(vol.call(obj));
+    return { area: areaObj, volume: volumeObj };
+  }
+
+  return objects.map(calc);
+}
+
+function area() {
+  return Math.abs(this.x * this.y);
+}
+
+function vol() {
+  return Math.abs(this.x * this.y * this.z);
+}
+
+console.log(
+  solve(
+    area,
+    vol,
+    `[
+{"x":"10","y":"-22","z":"10"},
+{"x":"47","y":"7","z":"-5"},
+{"x":"55","y":"8","z":"0"},
+{"x":"100","y":"100","z":"100"},
+{"x":"55","y":"80","z":"250"}
+]`
+  )
+);
---
+++ 06.Advanced Functions - Lab/02.add.js
@@ -0,0 +1,8 @@
+function solution(number) {
+  let result = number;
+
+  return result + number;
+}
+
+let add5 = solution(5);
+console.log(add5(7));

264395e5ed61cb8c325b47cdb3e4c92662098109
Roman Hristozov <roman.hristozov@gmail.com>
2025-02-07 12:38:45 +0200

    1 exerise

--- 09.Prototypes and Inheritance - Lab/05.classHierarchy.js
+++ 09.Prototypes and Inheritance - Lab/05.classHierarchy.js
@@ -1,25 +1,73 @@
-function result() {
-    class Figure {}
-    class Circle extends Figure {}
-    class Rectangle extends Figure {}
-
-    return { Figure, Circle, Rectangle}
-}
-
-
-
-let c = new Circle(5);
-console.log(c.area); // 78.53981633974483
-console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
-
-let r = new Rectangle(3, 4, 'mm');
-console.log(r.area); // 1200 
-console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
-
-r.changeUnits('cm');
-console.log(r.area); // 12
-console.log(r.toString()); // Figures units: cm Area: 12 - width: 3, height: 4
-
-c.changeUnits('mm');
-console.log(c.area); // 7853.981633974483
-console.log(c.toString()) // Figures units: mm Area: 7853.981633974483 - radius: 50
+function classHierarchy() {
+    class Figure {
+      constructor(units = 'cm') {
+        this.units = units;
+      }
+      // getter
+      get area() {
+        return this.area;
+      }
+      // methods
+      changeUnits(value) {
+        this.units = value;
+      }
+      toString() {
+        return `Figures units: ${this.units}`;
+      }
+      convertUnits(value) {
+        if (this.units == 'mm') {
+          value *= 10;
+        } else if (this.units == 'm') {
+          value /= 10;
+        }
+        return value;
+      }
+    }
+    class Circle extends Figure {
+      constructor(radius, units) {
+        super(units);
+        this._radius = Number(radius);
+      }
+      // radius getter
+      get radius() {
+        return this.convertUnits(this._radius);
+      }
+      //area getter
+      get area() {
+        return Math.PI * this.radius * this.radius;
+      }
+      // overrides method toString
+      toString() {
+        return super.toString() + ` Area: ${this.area} - radius: ${this.radius}`;
+      }
+    }
+    class Rectangle extends Figure {
+      constructor(width, height, units) {
+        super(units);
+        this._width = Number(width);
+        this._height = Number(height);
+      }
+      // getters
+      get width() {
+        return this.convertUnits(this._width);
+      }
+      get height() {
+        return this.convertUnits(this._height);
+      }
+      get area() {
+        return this.width * this.height;
+      }
+      // methods
+      toString() {
+        return (
+          super.toString() + ` Area: ${this.area} - width: ${this.width}, height: ${this.height}`
+        );
+      }
+    }
+  
+    return {
+      Figure,
+      Circle,
+      Rectangle,
+    };
+  }

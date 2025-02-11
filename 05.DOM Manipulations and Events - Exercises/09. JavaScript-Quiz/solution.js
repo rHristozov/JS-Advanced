ac1b5e6147fddcf606b065755840bab23e610350
Roman Hristozov <roman.hristozov@gmail.com>
2025-02-11 13:34:20 +0200

    More exercises

--- 05.DOM Manipulations and Events - Exercises/09. JavaScript-Quiz/solution.js
+++ 05.DOM Manipulations and Events - Exercises/09. JavaScript-Quiz/solution.js
@@ -1,9 +1,31 @@
 function solve() {
+  const sections = document.querySelectorAll('section');
+  const resultUl = document.getElementById('results');
+  const resultEl = document.querySelector('.results-inner h1');
+  let count = 0;
+  let correctAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'
+    ];
+    let correctAnswersCount = 0;
   document.body.addEventListener('click', (e) => {
-    let count = 0;
     if (e.target.tagName == 'P') {
-      console.log(e.target.textContent);
-      // e.target.tagName.textContent;
+      const answer = e.target.textContent;
+      if(correctAnswers.includes(answer)) {
+        correctAnswersCount++;
+      }
+      
+      if(count !== 2) {
+        sections[count].style.display = 'none';
+        sections[count + 1].style.display = 'block';
+        count++;
+      } else {
+        sections[count].style.display = 'none';
+        resultUl.style.display = 'block';
+        if(correctAnswersCount === 3) {
+          resultEl.textContent = 'You are recognized as top JavaScript fan!';
+        } else {
+          resultEl.textContent = `You have ${correctAnswersCount} right answers`
+        }
+      }
     }
   });
 }
--- 06.Advanced Functions - Exercises/10. Central Cinema/solution.js
+++ 06.Advanced Functions - Exercises/10. Central Cinema/solution.js
@@ -1,3 +1,74 @@
 function solve() {
-    // TODO
+    const onScreenBtn = document.querySelector('#container button')
+    const nameInputEl = document.querySelector('input[placeholder="Name"');
+    const hallInputEl = document.querySelector('input[placeholder="Hall"');
+    const ticketInputEl = document.querySelector('input[placeholder="Ticket Price"');
+    const ulMovies = document.querySelector('#movies ul');
+    const ulArchive = document.querySelector('#archive ul');
+    
+    onScreenBtn.addEventListener('click', onScreen);
+
+    function onScreen(e) {
+        e.preventDefault();
+        const movieName = nameInputEl.value;
+        const movieHall = hallInputEl.value;
+        let moviePrice = ticketInputEl.value;
+        moviePrice = Number(moviePrice);
+
+        if(movieName === '' || movieHall === '' || moviePrice === '' || typeof moviePrice !== 'number' ) {
+            return;
+        }
+
+        const liElMovies = document.createElement('li');
+
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = movieName;
+        const strongHall = document.createElement('strong');
+        strongHall.textContent = movieHall;
+
+        const divEl = document.createElement('div');
+        const strongPrice = document.createElement('strong');
+        strongPrice.textContent = moviePrice.toFixed(2);
+        let inputEl = document.createElement('input');
+        inputEl.placeholder = 'Tickets Sold';
+        const btnArchive = document.createElement('button');
+        btnArchive.textContent = 'Archive';
+
+        divEl.append(strongPrice, inputEl, btnArchive);
+        liElMovies.append(nameSpan, strongHall, divEl);
+        ulMovies.appendChild(liElMovies);
+
+        nameInputEl.value = '';
+        hallInputEl.value = '';
+        ticketInputEl.value = '';
+
+        btnArchive.addEventListener('click', archive)
+
+        function archive() {
+            console.log('here');
+            
+            const ticketPrice = Number(inputEl.value);
+            if(ticketPrice <= 0 ) {
+                return;
+            }
+            const liElArchive = document.createElement('li');
+
+            const nameSpan = document.createElement('span');
+            nameSpan.textContent = movieName;
+            const strongTotal = document.createElement('strong')
+            strongTotal.textContent = `Total amount: ${(ticketPrice * moviePrice).toFixed(2)}`
+            const btnDelete = document.createElement('button');
+            btnDelete.textContent =  'Delete';
+
+            liElArchive.append(nameSpan, strongTotal, btnDelete);
+            ulMovies.remove(liElMovies)
+            ulArchive.append(liElArchive);
+
+            btnDelete.addEventListener('click', remove)
+
+            function remove() {
+                ulArchive.remove(liElArchive)
+            }
+        }
+    }
 }
